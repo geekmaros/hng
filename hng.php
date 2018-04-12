9edd892f3890bc86adb7aa37d24a18b32acd2838
Mustapha Abdul-Rasaq <razicruz@gmail.com>
2018-04-12 11:58:23 +0100

    hng internship

---
+++ hng.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+
+	@font-face {
+    font-family: myFirstFont;
+    src: url(Sansation_Light.woff);
+}
+
+div {
+    font-family: myFirstFont;
+     margin: 180px;
+     text-align: 
+}
+		
+	h1{
+		color: #39FF08;
+		font-family: myFirstFont;
+	}
+
+
+	</style>
+</head>
+<body style="background-image: url(https://cdn.pixabay.com/photo/2017/05/13/15/23/cycling-2309811_960_720.jpg); background-size: cover;
+background-repeat: no-repeat;">
+
+<div style="margin: 230px; padding-left: 300px; " >
+	<h1 style="font-size: 24px;">HNG 4.0 INTERNSHIP</h1>
+	<h1 style="text-align: left; padding-left:10px;font-size: 64px;"><?php echo date("h:i a"); ?></h1>
+
+</div>
+
+</body>
+</html>
